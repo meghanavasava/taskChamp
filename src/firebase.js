e5e57d8f968a7aa32711295ad8eda9c775f5f026
ayesha1209 <116152639+ayesha1209@users.noreply.github.com>
2024-08-31 17:02:08 +0530

    Add files via upload

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCE7S6AyqYRuk3ACgQDlBdZOqzVFhGLGEs",
+  authDomain: "taskchamp-82e3d.firebaseapp.com",
+  projectId: "taskchamp-82e3d",
+  storageBucket: "taskchamp-82e3d.appspot.com",
+  messagingSenderId: "579272575280",
+  appId: "1:579272575280:web:faa504b3ca7265cdf4a0f5",
+  measurementId: "G-MZYN075T1V"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
