2bae856ce8f980f8328479a83f608cc723fec05b
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-09-01 12:46:20 +0530

    Save method in User & Task

--- src/Components/MyComponent.js
+++ src/Components/MyComponent.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { ref, set } from "firebase/database";
+import { realDb } from "../firebase";
 
 const MyComponent = () => {
   const handleClick = () => {
-    console.log("Button was clicked!");
+    const data = {
+      message: "Button was clicked!",
+      timestamp: new Date().toISOString(),
+    };
+
+    const newEntryId = Date.now(); // Or use another method to generate a unique ID
+
+    const dataRef = ref(realDb, "clicks/" + newEntryId);
+
+    set(dataRef, data)
+      .then(() => {
+        console.log("Data added successfully!");
+      })
+      .catch((error) => {
+        console.error("Error adding data:", error);
+      });
   };
 
   return (
