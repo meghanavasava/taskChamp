1168c048044ee45c0fe3e12308e479a4960b6434
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-09-01 12:52:13 +0530

    Record Added Successfully 😭

--- src/App.js
+++ src/App.js
@@ -5,27 +5,22 @@ import MyComponent from "./Components/MyComponent";
 //import { setupUserAndTask } from "./FirebaseOperations";
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
   return (
-    <div>
-      <MyComponent />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
