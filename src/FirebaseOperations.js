ee3d20a640d230064ba298d52fa434301ffa453f
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-09-17 15:11:40 +0530

    Push new Date User

--- src/FirebaseOperations.js
+++ src/FirebaseOperations.js
@@ -1,21 +1,19 @@
-import { ref, push, set } from "firebase/database";
+import { ref, set } from "firebase/database";
 import { realDb } from "./firebase";
 
-// Function to create a user in Firebase without explicit userId
 export const createUserInFirebase = (user) => {
-  const userRef = ref(realDb, "users"); // 'users' collection in the database
-  const newUserRef = push(userRef); // Generate unique ID for the user
+  const userId = `user_${Date.now()}`;
+  const userRef = ref(realDb, `users/${userId}`);
 
-  return set(newUserRef, user)
+  return set(userRef, user)
     .then(() => {
-      console.log("User created successfully!");
+      console.log("User created successfully with userId:", userId);
     })
     .catch((error) => {
       console.error("Error creating user:", error);
     });
 };
 
-// Function to add a task to a user in Firebase
 export const addTaskToUserInFirebase = (userId, task) => {
   const taskRef = ref(realDb, `users/${userId}/tasks/${task.taskId}`);
   return set(taskRef, task)
