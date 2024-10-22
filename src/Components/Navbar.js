2e4636f6434a4b405f255f058053b49f9a34e98b
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-10-22 18:51:42 +0530

    About

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
@@ -5,12 +6,36 @@ const Navbar = () => {
     <div className={styles.navbar}>
       <nav>
         <ul>
-          <li>Dashboard</li>
-          <li>My Activity</li>
-          <li>Leadership Board</li>
-          <li>Register</li>
-          <li>Login</li>
-          <li>About Us</li>
+          <li>
+            <Link to="/dashboard" className={styles.nav_link}>
+              Dashboard
+            </Link>
+          </li>
+          <li>
+            <Link to="/MyActivity" className={styles.nav_link}>
+              My Activity
+            </Link>
+          </li>
+          <li>
+            <Link to="/LeaderBoard" className={styles.nav_link}>
+              Leadership Board
+            </Link>
+          </li>
+          <li>
+            <Link to="/Registration" className={styles.nav_link}>
+              Register
+            </Link>
+          </li>
+          <li>
+            <Link to="/Login" className={styles.nav_link}>
+              Login
+            </Link>
+          </li>
+          <li>
+            <Link to="/AboutUs" className={styles.nav_link}>
+              About Us
+            </Link>
+          </li>
         </ul>
       </nav>
     </div>
